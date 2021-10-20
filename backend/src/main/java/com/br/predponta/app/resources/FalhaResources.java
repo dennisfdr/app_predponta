449a6c10610b3f01eaef64f80d3107cc9878f05e
Dennis Ribeiro <dennis_fdr@hotmail.com>
2021-10-20 12:20:05 -0300

    Atualização Swagger

--- backend/src/main/java/com/br/predponta/app/resources/FalhaResources.java
+++ backend/src/main/java/com/br/predponta/app/resources/FalhaResources.java
@@ -4,9 +4,8 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +19,20 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.br.predponta.app.dto.FalhaDTO;
 import com.br.predponta.app.servicies.FalhaService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping(value= "/falhas")
+@RequestMapping(value= "/api")
+@Api(value="API REST Falhas")
+@CrossOrigin(origins = "*")
+
 public class FalhaResources {
 	
 	@Autowired
 	private FalhaService service;
 	
+	@ApiOperation(value="Busca todas as falhas")
 	@GetMapping
 	public ResponseEntity<List<FalhaDTO>> findAll(){
 		List <FalhaDTO> list = service.findAll();
@@ -40,13 +46,16 @@ public class FalhaResources {
      //   Page <FalhaDTO> list = service.findAllPaged(pageable);
      //   return ResponseEntity.ok().body(list);
     //    }
-		
+	
+	
+	@ApiOperation(value="Busca Falha por ID")
 	@GetMapping(value = "/{fal_codigo}")
 	public ResponseEntity<FalhaDTO> findById(@PathVariable Long fal_codigo){
 		FalhaDTO dto = service.findById(fal_codigo);
 			return ResponseEntity.ok().body(dto);	
 	}
 	
+	@ApiOperation(value="Salva Falha")
 	@PostMapping
 	public ResponseEntity<FalhaDTO> insert(@RequestBody FalhaDTO dto){
 		dto = service.insert (dto);
@@ -56,12 +65,14 @@ public class FalhaResources {
 				return ResponseEntity.created(uri).body(dto);
 	}
 	
+	@ApiOperation(value="Atualia falha")
 	@PutMapping(value = "/{fal_codigo}")
 	public ResponseEntity<FalhaDTO> update(@PathVariable Long fal_codigo,@RequestBody FalhaDTO dto){
 		dto = service.update (fal_codigo, dto);
 		return ResponseEntity.ok().body(dto);
 	}			
 	
+	@ApiOperation(value="Deleta falha")
 	@DeleteMapping(value = "/{fal_codigo}")
 	public ResponseEntity<Void> delete(@PathVariable Long fal_codigo){
 		service.delete (fal_codigo);
